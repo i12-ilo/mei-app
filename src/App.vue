579c062286822ecf839cb26ffd59e043c5cefa00
xmt <3531278298@qq.com>
2020-04-13 09:42:30 +0800

    ''

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.el-main {
+  padding: 0 !important;
+  overflow: unset !important;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    publicPath: './',
+    outputDir: 'dist',
+    lintOnSave: true,
+    runtimeCompiler: true, 
+}
